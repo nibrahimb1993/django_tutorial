7f1ced44ba27394f2ff7b9ae6d751086e9abb059
nibrahimb1993 <nibrahimb1993@gmail.com>
2020-10-02 09:57:01 +0300

    customize question admin

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from polls.models import Question, Choice
 
-admin.site.register(Question)
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        "question_text",
+        "pub_date"
+    )
+    list_per_page = 2
+
+
 admin.site.register(Choice)
